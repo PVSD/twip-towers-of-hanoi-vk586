d7a75befcee7937c0991800a8454853da9986738
Valentina Kononenko <valentina@kononenko.net>
2019-01-22 00:05:10 -0500

    .

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String args[])
+    {
+        System.out.print("Tower of Hanoi Game." +
+                "\nThis puzzle will solve this puzzle for you." +
+                "\nEnter number of discs: ");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println("Instructions:");
+        TowersOfHanoi h = new TowersOfHanoi(n, 'A', 'B', 'C');
     }
 }
+
